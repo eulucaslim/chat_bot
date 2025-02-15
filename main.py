abae1c6eab8b14f6dbf769ab4a90e6aa00fab63a
eulucaslim <lucaslimacnt14@gmail.com>
2025-02-15 18:48:01 -0400

    webhook created

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
-waha = WahaAPI()
 gemini  = Gemini()
 
 @app.post("/send_message")
 def send_message_whatsapp(user: User):
     try:
-        response = waha.send_message(user.phone_number, user.text)
-        return JSONResponse(content={"msg": str(response)}, status_code=200)
+        response = WahaAPI.send_message(user.phone_number, user.text)
+        return JSONResponse(content=response, status_code=200)
     except Exception as e:
         print(e)
+
+@app.get("/get_webhook")
+def get_webhook():
+    WahaAPI.try_webhook()
