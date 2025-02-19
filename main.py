0977d7c0bae66195bb800557ff7d22dc476acdfc
eulucaslim <lucaslimacnt14@gmail.com>
2025-02-18 22:34:24 -0400

    create a function to save messages in database

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from app.lib.Gemini import Gemini
 from app.lib.WahaAPI import WahaAPI
+from app.lib.DataBase import DataBase
 from app.models.User import User
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
@@ -12,6 +13,8 @@ gemini  = Gemini()
 async def receive_message(request: Request):
     try:
         response = await request.json()
-        return JSONResponse(content=json.loads(response), status_code=200)
+        DataBase.save_messages(response)
+        del response["_id"]
+        return JSONResponse(content=response, status_code=200)
     except Exception as e:
         print(e)
