9e08490cca9048fd6da43dc2cfe7754325e62474
eulucaslim <lucaslimacnt14@gmail.com>
2025-02-18 22:51:19 -0400

    generate response in gemini

--- main.py
+++ main.py
@@ -13,8 +13,14 @@ gemini  = Gemini()
 async def receive_message(request: Request):
     try:
         response = await request.json()
+        ia_response = gemini.generate_response(response["payload"]["body"])
         DataBase.save_messages(response)
         del response["_id"]
+        WahaAPI.reply(
+            response["payload"]["from"], 
+            response["payload"]["id"], 
+            ia_response
+        )
         return JSONResponse(content=response, status_code=200)
     except Exception as e:
         print(e)
