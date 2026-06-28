854d19501a160a3107d0c395abfd4ad313ec0362
Lucas Lima <lucaslimacnt14@gmail.com>
2026-06-28 19:25:52 -0400

    fix: update router

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(
     version="0.0.1"
 )
 
-app.include_router(messages.router, prefix="/api/v1")
+app.include_router(messages.router)
 
 if __name__ == "__main__":
 	uvicorn.run(app, host="0.0.0.0", port=3000)
